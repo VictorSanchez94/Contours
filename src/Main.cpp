e28c51daa64b3409aa9faa180e629bf6e63df77c
cepero <cperito94@gmail.com>
2016-04-13 12:23:01 +0200

    Medio funciona

--- src/Main.cpp
+++ src/Main.cpp
@@ -30,40 +30,17 @@ int main( int argc, char** argv )
   Mat grad_x_impr, grad_y_impr, module_impr, orientation_impr;
 
   //Gradiente X
-  Sobel( src, grad_x, CV_64F, 1, 0, 3 );
-  //grad_x2 = grad_x/2 + 128;
-  //grad_x2.convertTo(grad_x2, CV_8U);
+  Sobel( src, grad_x, CV_32F, 1, 0, 3 );
 
   //Gradiente Y
-  Sobel( src, grad_y, CV_64F, 0, 1, 3 );
-  grad_y = - grad_y;
-  //grad_y2 = grad_y2/2 + 128;
-  //grad_y2.convertTo(grad_y2, CV_8U);
+  Sobel( src, grad_y2, CV_32F, 0, 1, 3 );
+  grad_y = - grad_y2;
 
   //Modulo
   magnitude(grad_x, grad_y, module);
-  //module.convertTo(module, CV_8U);
 
   //Orientacion
   phase(grad_x, grad_y, orientation);
-  /*if(in_degrees){
-	  orientation2 = orientation/180*128;
-  }else{
-	  orientation2 = orientation/3.1415*128;
-  }*/
-  //orientation2 = orientation;
-  //orientation2.convertTo(orientation2, CV_8U);
-
-  /*int count = 0;
-  for (int i=0; i<orientation.rows; i++){
-	  for (int j=0; j<orientation.cols; j++){
-		  float a = orientation.at<double>(i,j);
-		  cout << a << "   ";
-	  }
-	  cout << endl;
-	  count++;
-  }
-  cout << "NUMFILAS = " << count;*/
 
   /* Metodo directo. Se comprueban todos los puntos de la imagen y si su modulo es mayor
    * que un umbral, se crea su recta con su orientacion para intersectar con la linea del
@@ -71,9 +48,9 @@ int main( int argc, char** argv )
   int umbral = 95;
   for(int i=0; i<src.rows; i++){
 	  for(int j=0; j<src.cols; j++){
-		  double mod = module.at<double>(i,j);
+		  double mod = module.at<float>(i,j);
 		  if(mod > umbral){
-			  double orient = orientation.at<double>(i,j);
+			  double orient = orientation.at<float>(i,j);
 			  double pendiente = tan(orient);
 
 			  //Se construyen las ecuaciones de las dos rectas
@@ -85,7 +62,7 @@ int main( int argc, char** argv )
 			  //x = src.rows/2 + mx0 - y0
 			  int votado = src.rows/2 + pendiente*i - j;
 			  if(votado>=0 && votado<=src.rows){
-				  horizont.at<double>(votado) = horizont.at<double>(votado) + 1;
+				  horizont.at<uchar>(votado) = horizont.at<uchar>(votado) + 1;
 			  }
 
 		  }
@@ -95,30 +72,24 @@ int main( int argc, char** argv )
   //Se mira a ver cual ha sido el punto del horizonte mas votado
   int masVotado = 2;
   for (int i=0; i<horizont.cols; i++){
-	  if(horizont.at<double>(i) > horizont.at<double>(masVotado)){
+	  if(horizont.at<uchar>(i) > horizont.at<uchar>(masVotado)){
 		  masVotado = i;
 	  }
   }
 
-  //Mat final;
-  //src.copyTo(final);
-  /*for (int i=0; i<src.rows; i++){
-	  for (int j=0; j<src.cols; j++){
-		  if(i==src.rows/2){
-			  //final.at<int>(final.rows/2, j) = 0;
-		  }
-	  }
-  }*/
-  /*for(int i = -10; i<10; i++){
-	  final.at<double>(final.rows/2+i, masVotado) = 254;
-  }*/
+  Mat final;
+  src.copyTo(final);
+  for(int i = -10; i<10; i++){
+	  final.at<float>(final.rows/2+i, masVotado) = 254;
+	  final.at<float>(final.rows/2, masVotado+i) = 254;
+  }
 
 
   //Se convierten las matrices para que se puedan mostrar por pantalla
   grad_x2 = grad_x/2 + 128;
   grad_y2 = grad_y/2 + 128;
   orientation2 = orientation/3.1415*128;
-  /*grad_x2.convertTo(grad_x_impr, CV_8U);
+  grad_x2.convertTo(grad_x_impr, CV_8U);
   grad_y2.convertTo(grad_y_impr, CV_8U);
   module.convertTo(module_impr, CV_8U);
   orientation2.convertTo(orientation_impr, CV_8U);
@@ -128,8 +99,8 @@ int main( int argc, char** argv )
   imshow( "Gradiente_X", grad_x_impr );
   imshow( "Gradiente_Y", grad_y_impr );
   imshow( "Modulo", module_impr);
-  imshow( "Orientation", orientation_impr);*/
-  //imshow( "Final", final);
+  imshow( "Orientation", orientation_impr);
+  imshow( "Final", final);
 
   waitKey(0);
 
